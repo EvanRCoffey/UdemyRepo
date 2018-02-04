fe99f20b89637c33e006579302ff51bb329cacd7
R. Evan Coffey <rogerecoffey@gmail.com>
2018-02-04 15:40:34 -0600

    Quick change to reducers/reducer_weather

--- ReduxSimpleStarterThree/src/reducers/reducer_weather.js
+++ ReduxSimpleStarterThree/src/reducers/reducer_weather.js
@@ -1,5 +1,12 @@
-export default function(state = null, action) {
-	console.log('Action received', action);
+import { FETCH_WEATHER } from '../actions/index';
 
+export default function(state = [], action) {
+	switch(action.type) {
+	case FETCH_WEATHER:
+		//Ensure that you return a whole new instance of state
+		return state.concat([action.payload.data]);
+		//This ES6 line works too:
+		//return [action.payload.data, ...state];
+	}
 	return state;
 }
