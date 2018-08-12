858e7e888b7ba562ca2bd5a7db05e7ff63f12387
R. Evan Coffey <rogerecoffey@gmail.com>
2018-08-12 14:35:12 -0500

    I just copy and pasted the correct version of posts_new to get back on track.

--- ReduxSimpleStarterFour/src/components/posts_new.js
+++ ReduxSimpleStarterFour/src/components/posts_new.js
@@ -1,43 +1,22 @@
-<<<<<<< HEAD
-//Let's try one more time...
-=======
-//Just want to make sure I'm using git reset correctly...
->>>>>>> eb8aa8b9b81e8b5541ab81149eb46430ebedbd51
-
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
 	renderField(field) {
-		const { meta: { touched, error } } = field;		//ES6
-		const className = `form-group ${touched && error ? 'has-danger' : ''}`;
-
 		return (
-			<form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
-				<div className="className has-danger">
-					<label>{field.label}</label>
-					<input
-						className="form-control"
-						type="text"
-						{...field.input}	// Handling several events
-					/>
-					<div className="text-help">
-						{touched ? error : ''}
-					</div>
-					{field.meta.error}
-				</div>
-			</form>
+			<div className="form-group">
+				<label>{field.label}</label>
+				<input
+					className="form-control"
+					type="text"
+					{...field.input}	// Handling several events
+				/>
+				{field.meta.error}
+			</div>
 		);
 	}
 
-	onSubmit(values) {
-		// this === component
-		console.log(values);
-	}
-
 	render() {
-		const { handleSubmit } = this.props;
-
 		return (
 			<form>
 				<Field
@@ -55,7 +34,6 @@ class PostsNew extends Component {
 					name="content"
 					component={this.renderField}
 				/>
-				<button type="submit" className="btn btn-primary">Submit</button>
 			</form>
 		);
 	}
